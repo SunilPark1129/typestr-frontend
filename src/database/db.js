ff920b25b1ad3b26584d636518f27b941a5432ec
Sunil Park <sunilpark1129@gmail.com>
2024-04-27 15:58:20 -0400

    github testing

--- src/database/db.js
+++ src/database/db.js
@@ -7,43 +7,43 @@ Sunil Park
 import axios from "axios";
 
 export const postRank = (payload) => {
-    axios({
-            url: "/api/save",
-            method: "POST",
-            data: payload,
-        })
-        .then(() => {
-            console.log("Data has been sent to the server");
-        })
-        .catch(() => {
-            console.log("Internal server error");
-        });
+  axios({
+    url: "/openai/save",
+    method: "POST",
+    data: payload,
+  })
+    .then(() => {
+      console.log("Data has been sent to the server");
+    })
+    .catch(() => {
+      console.log("Internal server error");
+    });
 };
 
 export const getRank = (setData, setStatus) => {
-    axios
-        .get("/api")
-        .then((response) => {
-            const data = response.data;
-            data.sort((a, b) => {
-                return a.total - b.total;
-            });
-            setData(data);
-            setStatus(200);
-        })
-        .catch((e) => {
-            setStatus(e.response.status);
-            console.log("Error retrieving data");
-        });
+  axios
+    .get("/openai")
+    .then((response) => {
+      const data = response.data;
+      data.sort((a, b) => {
+        return a.total - b.total;
+      });
+      setData(data);
+      setStatus(200);
+    })
+    .catch((e) => {
+      setStatus(e.response.status);
+      console.log("Error retrieving data");
+    });
 };
 
 export const deleteRank = (id) => {
-    axios
-        .delete(`/api/delete/${id}`)
-        .then(() => {
-            console.log("Data has been deleted");
-        })
-        .catch(() => {
-            console.log("Error deleting data");
-        });
+  axios
+    .delete(`/openai/delete/${id}`)
+    .then(() => {
+      console.log("Data has been deleted");
+    })
+    .catch(() => {
+      console.log("Error deleting data");
+    });
 };
